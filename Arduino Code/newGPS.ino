ca3765df44a0b08c730dae257ebbe6447bc83659
Jsings <55395994+Jsings@users.noreply.github.com>
2019-12-06 18:31:45 -0500

    Update newGPS.ino

--- Arduino Code/newGPS.ino
+++ Arduino Code/newGPS.ino
@@ -1,22 +1,22 @@
 #include <Adafruit_GPS.h>
-//We can now create our Software Serial object after including the library
+// Software Serial object after including the library
 SoftwareSerial mySerial(3, 2);
 
-//And finally attach our Serial object pins to our GPS module
+//attach Serial object pins to GPS module
 Adafruit_GPS GPS(&mySerial);
 
 void setup() {
-Serial.begin(9600); //This baud rate will help a lot in printing all of the data that comes from the GPS Module to the serial monitor
+Serial.begin(9600); 
 GPS.begin(9600); 
 //These lines configure the GPS Module
 GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Sets output to only RMC and GGA sentences
-GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //Sets the output to 1/second. If you want you can go higher/lower
-GPS.sendCommand(PGCMD_ANTENNA); //Can report if antenna is connected or not
+GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //Sets the output to 1/second.
+GPS.sendCommand(PGCMD_ANTENNA); //report if antenna is connected or not
 }
 
 void loop() {
-//Now we will start our GPS module, parse (break into parts) the Last NMEA sentence 
-GPS.parse(GPS.lastNMEA()); //This is going to parse the last NMEA sentence the Arduino has received, breaking it down into its constituent parts.
+//start our GPS module, parse last received sentence 
+GPS.parse(GPS.lastNMEA()); //This is going to parse the last NMEA sentence the Arduino has received
 GPS.newNMEAreceived(); //This will return a boolean TRUE/FALSE depending on the case.
 //Print the current date/time/etc
     Serial.print("\nTime: ");
